d8c474e85769e1f6597166a4c6ff1dac47028d20
Kaan Pinarci <kapinarc@z4r3p7.42lyon.fr>
2025-10-14 17:48:27 +0200

    begin norm raycast

--- src/exec/raycast.c
+++ src/exec/raycast.c
@@ -4,12 +4,11 @@
 
 double	y_inter(t_data *data, double angle)
 {
-	double x;
-	double y;
-	double x_step;
-	double y_step;
-	int pixel;
-
+	double	x;
+	double	y;
+	double	x_step;
+	double	y_step;
+	int		pixel;
 
 	x_step = TILE;
 	y_step = TILE * tan(angle);
@@ -18,52 +17,43 @@ double	y_inter(t_data *data, double angle)
 	y = data->player->py + (x - data->player->px) * tan(angle);
 	if ((ray_dir(angle, 0) && y_step < 0) || (!ray_dir(angle, 0) && y_step > 0)) //rediriger lazer de y_step du bon cote en fontion de l'angle (droite ou gauche du cercle trigo)
 		y_step *= -1;
-	while (!is_wall(data->map, x  + pixel  , y))
+	while (!is_wall(data->map, x + pixel, y))
 	{
-		// printf("(fonction y_inter) [x + pixel] = %f [%d] | [y] = %f [%d]\n", x, (int)x + pixel, y, (int)y);
-		// printf("(fonction y_inter) [xstep] = %f [%d] | [ystep] = %f [%d]\n", x_step, (int)x, y_step, (int)y);
-		// mlx_pixel_put(data->mlx, data->win, (int)x  , (int)y, 0x00FF0000);
 		y += y_step;
 		x += x_step;
 	}
 	data->ray->rx = x;
 	data->ray->ry = y;
-//	return ((data->ray->ry - data->player->py) / sin(angle)); //pour corriger le fish-eyes pour x_inter
 	return (sqrt(pow(y - data->player->py, 2) + pow(x - data->player->px, 2)));
 }
 
 double	x_inter(t_data *data, double angle)
 {
-	double x_step;
-	double y_step;
-	double x;
-	double y;
-	int pixel;
+	double	x;
+	double	y;
+	double	x_step;
+	double	y_step;
+	int		pixel;
 
 	y_step = TILE;
 	x_step = TILE / tan(angle);
 	y = floor(data->player->py / TILE) * TILE;
 	pixel = balance_inter(angle, &y, &y_step, 0);
-	// printf("pixel = %d\n", pixel);
 	x = data->player->px + (y - data->player->py) / tan(angle);
 	if ((ray_dir(angle, 1) && x_step < 0) || (!ray_dir(angle, 1) && x_step > 0))
 		x_step *= -1;
 	while (!is_wall(data->map, x, y + pixel))
 	{
-		// printf("(fonction x_inter) [x] = %f [%d] | [y] = %f [%d]\n", x, (int)x , y, (int)y);
-		// printf("(fonction x_inter) boucle\n");
-		// mlx_pixel_put(data->mlx, data->win, (int)x  , (int)y, 0x00FF0000);
 		x += x_step;
 		y += y_step;
 	}
 	data->ray->rx = x;
 	data->ray->ry = y;
-//	return ((data->ray->rx - data->player->px) / cos(angle)); //pour corriger le fish-eyes pour y_inter
-	return (sqrt(pow(x -data->player->px , 2) + pow(y -data->player->py, 2)));
+	return (sqrt(pow(x - data->player->px, 2) + pow(y - data->player->py, 2)));
 }
+
 double	lazerizor(t_data *data, double angle)
 {
-
 	double	x_dist;
 	double	y_dist;
 
@@ -75,58 +65,15 @@ double	lazerizor(t_data *data, double angle)
 	return (x_dist);
 }
 
-
-void my_mlx_put_pixel(t_img *img, int x, int y, int color)
+void	my_mlx_put_pixel(t_img *img, int x, int y, int color)
 {
 	char	*pixel;
-
-    // Sécurité : vérifier que x et y sont dans les bornes
+	// Sécurité : vérifier que x et y sont dans les bornes
 	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
-		return;
+		return ;
 	pixel = img->addr + (y * img->line_length + x * (img->bpp / 8));
 	*(unsigned int *)pixel = color;
 }
-
-
-
-
-//void	raycaster(t_data *data)
-//{
-//	int		i;
-//	int		j;
-//	double	rad_fov;
-//	double	dist;
-//
-//	i = 0;
-//	rad_fov = FOV * (M_PI / 180);
-//	data->ray->angle = (data->player->angle - (rad_fov * 0.5));
-//	while (i < WIDTH)
-//	{
-//		j = 0;
-//		norm_angle(&data->ray->angle);
-//		dist = lazerizor(data, data->ray->angle);
-////		printf("(in %s ) dist = [%f]\n", __func__, dist);
-////		printf("(in %s) ray angle = [%f] |  player angle = [%f]\n", __func__, data->ray->angle, data->player->angle);
-////		if (dist > )
-//		while (j < HEIGHT)
-//		{
-//			norm_angle(&data->player->angle);
-//			dist *=  cos(data->ray->angle - data->player->angle);
-//			if (j < dist)
-//				my_mlx_put_pixel(data->img, i, j, 0x00FFFFFF);
-//			else if (j > HEIGHT - dist)
-//				my_mlx_put_pixel(data->img, i, j, 0x000000FF);
-//			else
-//				my_mlx_put_pixel(data->img, i, j, 0x00000000);
-//			j++;
-//		}
-//		data->ray->ray_dist = dist;
-//		data->ray->angle += rad_fov / WIDTH;
-//		i++;
-//	}
-//}
-
-
 
 void	raycaster(t_data *data)
 {
@@ -154,7 +101,6 @@ void	raycaster(t_data *data)
 			wall_height = HEIGHT;
 		wall_top = (HEIGHT - wall_height) / 2;
 		wall_bot = wall_top + wall_height;
-
 		j = 0;
 		while (j < HEIGHT)
 		{
