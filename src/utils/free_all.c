2d61af8f62dae6620316c1133cbb0094dc120dc3
Enzo <enzochevrier@icloud.com>
2025-10-16 00:40:44 +0200

    Refactor memory management: separate map freeing logic into its own function and streamline resource cleanup in free_all.

--- src/utils/free_all.c
+++ src/utils/free_all.c
@@ -1,28 +1,64 @@
 #include "cub3d.h"
 
+static void	free_map(t_map *map)
+{
+	if (!map)
+		return ;
+	if (map->fd_map >= 0)
+		safe_close(&map->fd_map);
+	free(map->file_name);
+	free(map->n_wall_path);
+	free(map->s_wall_path);
+	free(map->w_wall_path);
+	free(map->e_wall_path);
+	free(map->floor_color);
+	free(map->ceiling_color);
+	if (map->map)
+		free_tab_return_null(map->map);
+	free(map);
+}
+
+static void	free_images(t_data *data)
+{
+	if (data->img)
+	{
+		if (data->img->img && data->mlx)
+			mlx_destroy_image(data->mlx, data->img->img);
+		free(data->img);
+	}
+	if (data->texture)
+	{
+		if (data->texture->wall && data->mlx)
+			mlx_destroy_image(data->mlx, data->texture->wall);
+		if (data->texture->perso_n && data->mlx)
+			mlx_destroy_image(data->mlx, data->texture->perso_n);
+		free(data->texture);
+	}
+}
+
+static void	free_structs(t_data *data)
+{
+	if (data->player)
+		free(data->player);
+	if (data->ray)
+		free(data->ray);
+	if (data->key)
+		free(data->key);
+	if (data->map)
+		free_map(data->map);
+}
+
+static void	free_mlx(t_data *data)
+{
+	if (data->win && data->mlx)
+		mlx_destroy_window(data->mlx, data->win);
+}
+
 void	free_all(t_data *data)
 {
 	if (!data)
 		return ;
-	if (data->map)
-	{
-		safe_close(&data->map->fd_map);
-		free(data->map->file_name);
-		free(data->map->n_wall_path);
-		free(data->map->s_wall_path);
-		free(data->map->w_wall_path);
-		free(data->map->e_wall_path);
-		free(data->map->floor_color);
-		free(data->map->ceiling_color);
-		free(data->player);
-		if (data->map->map)
-			free_tab_return_null(data->map->map);
-		free(data->map);
-	}
-	if (data->ray)
-		free(data->ray);
-	if (data->mlx)
-		free(data->mlx);
-	if (data->texture)
-		free(data->texture);
+	free_images(data);
+	free_structs(data);
+	free_mlx(data);
 }
