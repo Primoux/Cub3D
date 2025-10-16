3b4114256d1d45ca5ddae55bdef9dc896b033044
enchevri <enzochevrier@icloud.com>
2025-10-16 16:24:46 +0200

    Remove unnecessary raycasting and minimap drawing calls from winner function

--- src/mlx/mlx_handler.c
+++ src/mlx/mlx_handler.c
@@ -10,8 +10,6 @@ void	winner(t_data *data)
 			&data->img->line_length, &data->img->endian);
 	mlx_hook(data->win, 2, 1L << 0, handle_press_key, data);
 	mlx_hook(data->win, 3, 1L << 1, handle_release_key, data);
-	raycaster(data);
-	draw_minimap(data);
 	mlx_put_image_to_window(data->mlx, data->win, data->img->img, 0, 0);
 	mlx_hook(data->win, 17, 0, close_window, data);
 	mlx_loop_hook(data->mlx, &move_player, data);
