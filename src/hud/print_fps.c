f0e02ab5e02220ba4ce348c134cdd58d732dabfc
enchevri <enzochevrier@icloud.com>
2025-10-30 10:19:57 +0100

    Refactor FPS display logic: streamline string handling and update rendering for improved clarity

--- src/hud/print_fps.c
+++ src/hud/print_fps.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:27:42 by enchevri          #+#    #+#             */
-/*   Updated: 2025/10/30 09:16:36 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/10/30 10:15:38 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	draw_fps(t_data *data, double current_time_s)
 	static double	last_update = 0.0;
 	static int		fps = 0;
 	char			*str;
+	char			*str2;
 
+	str = NULL;
 	frame_count++;
 	if (current_time_s - last_update >= 1.0)
 	{
@@ -28,12 +30,12 @@ void	draw_fps(t_data *data, double current_time_s)
 		last_update = current_time_s;
 	}
 	str = ft_itoa(fps);
-	if (str)
+	str2 = ft_strjoin("FPS ", str);
+	free(str);
+	if (str2)
 	{
-		mlx_string_put(data->mlx, data->win, WIDTH - (ft_strlen(str) * 6) - 25,
-			15, 0x00FFFFFF, "FPS ");
-		mlx_string_put(data->mlx, data->win, WIDTH - (ft_strlen(str) * 6) - 1,
-			15, 0x00FFFFFF, str);
-		free(str);
+		mlx_string_put(data->mlx, data->win, WIDTH - (ft_strlen(str2) * 6) - 3,
+			15, 0x00FFFFFF, str2);
+		free(str2);
 	}
 }
